8863ebd9f50662832d8a99be524729b8168d469b
jesperhauch <s174227@student.dtu.dk>
2022-11-20 17:22:29 -0800

    mlp draft and conv early draft

--- library/models/models.py
+++ library/models/models.py
@@ -1,20 +1,24 @@
-from flax import linen
-
-class ConvCustom(linen.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=2, padding=1, batch_norm=True):
-        super(ConvCustom, self).__init__()
-        self.conv_layer = linen.Conv(features=out_channels, kernel_size=kernel_size, strides=stride, padding=padding)
-        
-        if batch_norm:
-            self.batchnorm = linen.BatchNorm()
+from flax import linen as nn
 
 class CycleGAN:
     def __init__(self, generator: nn.Module, discriminator: nn.Module):
         loss = # TODO
         pass
 
+    def forward(self, x):
+        pass
+
+    def backward(self):
+        pass
+
 
 class GAN:
     def __init__(self, generator: nn.Module):
         loss = # TODO
+        pass
+
+    def forward(self, x):
+        pass
+    
+    def backward(Self, x):
         pass
--- work-separation.md
+++ work-separation.md
@@ -2,4 +2,5 @@
 
 - Trent: dataset generators
 - Jesper & AK: architectures.py, models.py
+    - Should we implement ResNet in Jax?
 - Tiger: training loops, visualizations
